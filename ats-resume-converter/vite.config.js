01cac98e552a815c9ee2ce24253278255bfd0af6
AC-Collins <alastor.c@outlook.com>
2025-04-21 00:21:54 +0000

    Resolved merge conflicts and merged gh-pages

--- ats-resume-converter/vite.config.js
+++ ats-resume-converter/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Moja-Kwa-Moja/',  // Add your repo name here
 })
